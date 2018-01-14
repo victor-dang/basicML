14451ec66abf2e17634aa71c2ea4a9a8c5cff3bf
unknown <thangddnt@gmail.com>
2018-01-14 21:54:06 +0900

    updated

--- example4/ex4/nnCostFunction.m
+++ example4/ex4/nnCostFunction.m
@@ -44,14 +44,17 @@ Y = zeros(m,num_labels);
 for i=1:m
 	Y(i,:)=I(y(i),:);
 end
-X = [ones(m, 1) X];
-activations1 = X;
-z2 = X*Theta1';
+%X = [ones(m, 1) X];
+activations1 = [ones(m, 1) X];
+z2 = activations1*Theta1';
 activations2 = sigmoid(z2);
 activations2 =[ones(m,1) activations2];
 z3 = activations2*Theta2';
 activations3 = sigmoid(z3);
+
 J = (1/m)*sum(sum((-Y).*log(activations3)-(1-Y).*log(1-activations3),2));
+regularization = lambda/(2*m)*(sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2)));
+J = J + regularization;
 
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
@@ -68,6 +71,27 @@ J = (1/m)*sum(sum((-Y).*log(activations3)-(1-Y).*log(1-activations3),2));
 %               over the training examples if you are implementing it for the 
 %               first time.
 %
+delta3 = activations3 - Y;
+delta2 = (delta3*Theta2.*sigmoidGradient([ones(size(z2,1),1) z2]))(:,2:end);
+%delta2 = delta2(2:end);
+%delta2 = 1/m.*delta2;
+%Delta_1 = zeros(hidden_layer_size,1)';
+%Delta_2 = zeros(num_labels,1)';
+%for t = 1:m 
+	% fprintf('\n iter: %d',t);
+	% a1(t,:) = [1 X(t,:)];
+	% %fprintf('\n a1: %d \n',a1);
+	% z2(t,:) = a1(t,:)*Theta1';
+	% a2(t,:) = [1 sigmoid(z2(t,:))];
+	% z3(t,:) = a2(t,:)*Theta2';
+	% a3(t,:) = sigmoid(z3(t,:));
+	% deta3(t,:) = a3(t,:) - Y(t,:);
+	% deta2(t,:) = deta3(t,:)*Theta2.*sigmoidGradient([1 z2(t,:)]);
+	% deta2 = deta2(t,2:end);
+	% fprintf('\n Delta_1: %d deta2: %d, a1: %d\nwhos',size(Delta_1,1),size(delta2,1),size(a1,1));
+	% Delta_1(t,:) = Delta_1(t,:)+deta2(t,:)*a1(t,:);
+	% Delta_2(t,:) = Delta_2(t,:)+deta3(t,:)*a2(t,:);
+%end
 % Part 3: Implement regularization with the cost function and gradients.
 %
 %         Hint: You can implement this around the code for
@@ -76,7 +100,13 @@ J = (1/m)*sum(sum((-Y).*log(activations3)-(1-Y).*log(1-activations3),2));
 %               and Theta2_grad from Part 2.
 %
 
+Delta_1 = delta2'*activations1;
+Delta_2 = delta3'*activations2;
 
+
+Theta1_grad = Delta_1./m + (lambda/m)*[zeros(size(Theta1,1), 1) Theta1(:, 2:end)];
+Theta2_grad = Delta_2./m + (lambda/m)*[zeros(size(Theta2,1), 1) Theta2(:, 2:end)];
+grad = [Theta1_grad(:) ; Theta2_grad(:)];
 
 
 
